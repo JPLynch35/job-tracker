ce29c63bf35637b3425c2ae81b563133f164836e
Matthew Peters <JiveTurkey@MattBook-Pro.local>
2018-07-07 18:11:45 -0600

    WIP categories

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   resources :companies do
     resources :jobs
   end
+
+  resources :categories
+
+  # get "/jobs", to: 'jobs#index'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
