2ba28629f209148f09d5d2214c1fd97d42e81d03
Matthew Peters <JiveTurkey@MattBook-Pro.local>
2018-07-08 14:57:51 -0600

    adds sort funtionality for location and interest

--- spec/features/jobs/user_sees_sorted_jobs_spec.rb
+++ spec/features/jobs/user_sees_sorted_jobs_spec.rb
@@ -5,10 +5,10 @@ describe "A User" do
     @category_1 = Category.create(title: "Finance")
     @category_2 = Category.create(title: "Definitely Not Finance")
     @company_1 = Company.create(name: "ESPN")
-    @job_11 = @company_1.jobs.create(title: "Developer", level_of_interest: 70, city: "Denver", category_id: @category_1.id)
-    @job_12 = @company_1.jobs.create(title: "QA Analyst", level_of_interest: 70, city: "New York City", category_id: @category_2.id)
+    @job_11 = @company_1.jobs.create(title: "Developer", level_of_interest: 4, city: "Denver", category_id: @category_1.id)
+    @job_12 = @company_1.jobs.create(title: "QA Analyst", level_of_interest: 3, city: "New York City", category_id: @category_2.id)
     @company_2 = Company.create(name: "Apple")
-    @job_21 = @company_2.jobs.create(title: "Manager", level_of_interest: 70, city: "Denver", category_id: @category_1.id)
+    @job_21 = @company_2.jobs.create(title: "Manager", level_of_interest: 2, city: "Denver", category_id: @category_1.id)
     @job_22 = @company_2.jobs.create(title: "Thug", level_of_interest: 1, city: "Orlando", category_id: @category_1.id)
   end
   scenario "sees all jobs sorted by City" do
@@ -23,8 +23,8 @@ describe "A User" do
     visit "/jobs?sort=interest"
 
 
-    expect(@job_11.title).to appear_before(@job_12.title)
-    expect(@job_21.title).to appear_before(@job_12.title)
-    expect(@job_12.title).to appear_before(@job_22.title)
+    expect(@job_22.title).to appear_before(@job_21.title)
+    expect(@job_22.title).to appear_before(@job_12.title)
+    expect(@job_21.title).to appear_before(@job_11.title)
   end
 end
