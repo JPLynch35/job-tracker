f8f38b43ad88d96acff3856cc752a3f004b72466
JP Lynch <JPLynch35@gmail.com>
2018-07-10 22:38:31 -0600

    Adds edit and delete buttons to jobs index

---
+++ spec/features/dashboard/user_can_navigate_website_with_navbar_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+describe 'a user visits /dashboard' do
+  before :each do
+    @category = Category.create(title: 'Finance')
+    @company_1 = Company.create(name: "ESPN")
+    @job_1 = @company_1.jobs.create(title: "Developer", level_of_interest: 2, city: "Denver", category_id: @category.id)
+  end
+  describe 'can navigate the website' do
+    scenario 'utilizing the navbar' do
+      visit dashboard_index_path
+
+      within '.nav' do
+        click_on 'Companies'
+      end
+
+      expect(current_path).to eq(companies_path)
+
+      within '.nav' do
+        click_on 'Jobs'
+      end
+
+      expect(current_path).to eq(jobs_path)
+
+      within '.nav' do
+        click_on 'Categories'
+      end
+
+      expect(current_path).to eq(categories_path)
+    end
+  end
+end
--- spec/features/jobs/user_sees_all_jobs_spec.rb
+++ spec/features/jobs/user_sees_all_jobs_spec.rb
@@ -16,7 +16,29 @@ describe "User sees all jobs" do
     expect(page).to have_content("QA Analyst")
     expect(page).to have_content("Manager")
     expect(page).to have_content("#{@job_11.title} at #{@company_1.name} - #{@job_11.city}")
+  end
+  scenario "a user clicks on sort by location and sees the sort" do
+    visit jobs_path
 
+    click_on "Sort all jobs by location"
+
+    expect(@job_11.title).to appear_before(@job_21.title)
+    expect(@job_21.title).to appear_before(@job_12.title)
+  end
+  scenario "a user clicks on sort by interest and sees the sort" do
+    visit jobs_path
+
+    click_on "Sort all jobs by interest"
+
+    expect(@job_12.title).to appear_before(@job_11.title)
+    expect(@job_11.title).to appear_before(@job_21.title)
+  end
+  scenario "a user clicks on a job title and sees the job show page" do
+    visit jobs_path
+
+    click_on @job_11.title
+
+    expect(current_path).to eq(job_path(@job_11))
   end
   scenario "a user sees all the jobs for a specific company" do
     visit company_jobs_path(@company_1)
